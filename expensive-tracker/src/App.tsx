631839fd0f3bb005105d8af6b3e73000ab9120b7
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-25 08:24:54 -0300

    items e item implementado V1

--- expensive-tracker/src/App.tsx
+++ expensive-tracker/src/App.tsx
@@ -7,7 +7,11 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        {/* Área de informações*/}
+
+        {/* Área de insrção*/}
+
+        {/* Tabela de itens*/}
       </C.Body>
     </C.Container>
   )
