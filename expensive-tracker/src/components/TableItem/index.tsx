6e747774ad3aae7501ee7a63c0c66f2b25efeeb2
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-25 10:58:48 -0300

    listando os itens e categorias implementado V2

--- expensive-tracker/src/components/TableItem/index.tsx
+++ expensive-tracker/src/components/TableItem/index.tsx
@@ -11,9 +11,17 @@ export const TableItem = ({ item }: Props) => {
     return (
         <C.TableLine>
             <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
-            <C.TableColumn>{categories[item.category].title}</C.TableColumn>
+            <C.TableColumn>
+                <C.Category color={categories[item.category].color}>
+                    {categories[item.category].title}
+                </C.Category>
+            </C.TableColumn>
             <C.TableColumn>{item.title}</C.TableColumn>
-            <C.TableColumn>R$ {item.value}</C.TableColumn>
+            <C.TableColumn>
+                <C.Value color={categories[item.category].expensive ? 'red' : 'green'}>
+                    R$ {item.value}
+                </C.Value>
+            </C.TableColumn>
         </C.TableLine>
     );
 }
