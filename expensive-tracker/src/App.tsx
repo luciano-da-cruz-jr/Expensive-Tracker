5cae0f59f963b963db1ad787ae3cfc70793e315c
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-25 09:16:52 -0300

    filteredlist implementado V1

--- expensive-tracker/src/App.tsx
+++ expensive-tracker/src/App.tsx
@@ -1,6 +1,20 @@
 import * as C from './App.styles';
+import { Item } from './type/Item';
+import { Category } from './type/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { useEffect, useState } from 'react';
+import { getCurrentMonth, filterListByMonth } from './helpers/dataFilter';
 
 const App = () => {
+  const [list, setList] = useState(items); // Lista completa
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList( filterListByMonth(list, currentMonth) );
+  }, [list, currentMonth])
+
   return (
     <C.Container>
       <C.Header>
@@ -12,6 +26,7 @@ const App = () => {
         {/* Área de insrção*/}
 
         {/* Tabela de itens*/}
+
       </C.Body>
     </C.Container>
   )
