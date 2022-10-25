298a13dbdb81e4955d912ec4ad9524462b3988d0
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-25 08:45:36 -0300

    categories e category implementado V1

---
+++ expensive-tracker/src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../type/Category";
+
+export const categories: Category = {
+    food: { title: 'Alimentação', color: 'blue', expensive: true},
+    rent: { title: 'Aluguel', color: 'brown', expensive: true},
+    salary: { title: 'Salário', color: 'green', expensive: true},
+}
